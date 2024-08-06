b5115f410d269fba78125c7661768af89ff129ef
Tarun V G <tarunvg2002@gmail.com>
2024-08-06 16:11:03 +0530

    Update portfolio.js

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -66,12 +66,8 @@ const experience = {
       title: "AI Engineer",
       company: "Go Academy/ Go Labs",
       duration: "April 2024 - present",
-      description:"• Object Detection Pipeline: Developed a YOLO-based pipeline for object detection in video frames, enabling keyword-based clip generation and summary creation, enhancing content analysis and annotation.<br>" +
-               "• Text Extraction & Summarization: Automated text extraction and summarization from PDFs and websites using open-source Large Language Models (LLMs) and advanced NLP techniques.<br>" +
-               "• Data Processing & Analysis Pipelines: Designed and optimized data pipelines using Python, MongoDB, and FastAPI, enhancing data handling and reporting efficiency. Applied data analysis and visualization techniques to generate insights, improving reporting accuracy.<br>" +
-               "• Blog Recommendation System: Created a scalable recommendation system with Dask and BERT embeddings for semantic text analysis. Implemented ChromaDB and Flask API for scalable data querying and dynamic recommendations.<br>" +
-               "• NLP & AI Exploration: Explored Llama 2, Llama 3, and LangChain for advanced NLP and AI applications."
-        /*
+      description:
+    `
         • Object Detection Pipeline: Developed a YOLO-based pipeline for object detection in video frames, enabling keyword-based clip
           generation and summary creation enhancing content analysis and annotation.
         • Text Extraction & Summarization: Automated text extraction and summarization from PDFs and websites using open-source
@@ -82,7 +78,7 @@ const experience = {
         • Blog Recommendation System: Created a scalable recommendation system with Dask and BERT embeddings for semantic text
           analysis. Implemented ChromaDB and Flask API for scalable data querying and dynamic recommendations.
         • NLP & AIExploration: Explored Llama 2, Llama 3, and LangChain for advanced NLP and AI applications.
-        */
+        `
     },
     
     {
